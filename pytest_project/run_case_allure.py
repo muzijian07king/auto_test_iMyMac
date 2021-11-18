4b8f46c52cae5a816c462ca168b250b2e2f5fd8a
muzijian07king <muzijian07king@163.com>
2021-11-18 18:01:00 +0800

    1118

--- pytest_project/run_case_allure.py
+++ pytest_project/run_case_allure.py
@@ -10,20 +10,17 @@ def main():
     """主函数"""
     steps = [
         "venv\\Script\\activate" if WIN else "",
-        "pytest --lf --alluredir allure-results --clean-alluredir" if WIN else "/usr/local/python39/bin/pytest "
-                                                                                                                     "--alluredir allure-results "
-                                                                                                                     "--clean-alluredir",
+        "pytest --alluredir allure-results --clean-alluredir" if WIN else "--alluredir allure-results "
+                                                                               "--clean-alluredir ",
         "copy utils\\environment.properties allure-results\\environment.properties" if WIN else 'cp utils/environment'
                                                                                                 '.properties '
                                                                                                 "allure-results"
                                                                                                 "/environment"
                                                                                                 '.properties',
-        "allure generate allure-results -c  -o report/allure" if WIN else "/usr/local/allure-2.13.6/bin/allure "
-                                                                          "generate allure-results -c  -o "
-                                                                          "report/allure",
-        "python pytest_history_trend.py" if HISTORY else "",
-        "python clear_download_dir.py" if DOWNLOAD else "",
-        "allure open report/allure " if False else ""
+        "allure generate allure-results -c  -o report/allure",
+        "python pytest_history_trend.py" if WIN else "",
+        "python clear_download_dir.py" if WIN else "",
+        "allure open report/allure " if WIN else ""
     ]
     for step in steps:
         subprocess.run("call " + step if WIN else step, shell=True)
