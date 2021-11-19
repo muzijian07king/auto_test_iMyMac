ef6147fe8d26204f5f10450370add78a87b36b16
muzijian07king <muzijian07king@163.com>
2021-11-19 09:14:08 +0800

    修改启动类

--- pytest_project/run_case_allure.py
+++ pytest_project/run_case_allure.py
@@ -5,19 +5,19 @@ WIN = sys.platform.startswith('win')
 HISTORY = True
 DOWNLOAD = True
 
-
+ 
 def main():
     """主函数"""
     steps = [
-        "venv\\Script\\activate" if WIN else "",
-        "pytest --alluredir allure-results --clean-alluredir" if WIN else "--alluredir allure-results "
+        "venv\\Script\\activate" if WIN else "source venv/bin/activate",
+        "pytest --alluredir allure-results --clean-alluredir" if WIN else "pytest --alluredir allure-results "
                                                                                "--clean-alluredir ",
         "copy utils\\environment.properties allure-results\\environment.properties" if WIN else 'cp utils/environment'
                                                                                                 '.properties '
                                                                                                 "allure-results"
                                                                                                 "/environment"
                                                                                                 '.properties',
-        "allure generate allure-results -c  -o report/allure",
+        "allure generate allure-results -c  -o report/allure" if WIN else '',
         "python pytest_history_trend.py" if WIN else "",
         "python clear_download_dir.py" if WIN else "",
         "allure open report/allure " if WIN else ""
