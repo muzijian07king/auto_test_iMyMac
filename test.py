26316e5807f44725a7049d6b621886766753f520
muzijian07king <muzijian07king@163.com>
2021-12-14 18:01:04 +0800

    单线程跑

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-import time
+import os
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome()
-driver.get('https://www.imymac.com/mac-cleaner/')
-driver.execute_script('document.documentElement.scrollTop=6000')
-time.sleep(1)
-driver.find_element_by_css_selector('body > div.footerBuy > div > div.flexEnd > div.text-center.productColumn.hide-visible.tipsup > div > a.btn.btn-dark.btn-buy').click()
-time.sleep(2)
-
-driver.close()
+print(os.system('ipconfig'))
