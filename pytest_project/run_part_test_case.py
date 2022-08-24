6518303c6108ff18fd48ceb070c87f79b17fe388
muzijian07king <muzijian07king@163.com>
2022-08-24 16:37:28 +0800

    增加运行部分用例启动文件

---
+++ pytest_project/run_part_test_case.py
@@ -0,0 +1,38 @@
+import sys
+import subprocess
+import argparse
+
+WIN = sys.platform.startswith('win')
+HISTORY = True
+DOWNLOAD = True
+DeBug = False
+
+
+def get_test_part():
+    parse = argparse.ArgumentParser(description='添加pytest参数')
+    parse.add_argument('--pytest', default='', nargs='?')
+    args = parse.parse_args()
+    return args.pytest
+
+
+def main(part):
+    """主函数"""
+    steps = [
+        f"pytest {part} --alluredir report\\data --clean-alluredir" if WIN else f'pytest {part} --alluredir report/data'
+                                                                                '--clean-alluredir',
+        "copy utils\\environment.properties report\\data\\environment.properties" if WIN else 'cp utils/environment'
+                                                                                              '.properties '
+                                                                                              "report/data"
+                                                                                              "/environment"
+                                                                                              '.properties',
+        "allure generate report\\data -c  -o report\\allure" if WIN else '',
+        "python pytest_history_trend.py" if WIN else "",
+        "python clear_download_dir.py",
+        "allure open report\\allure" if DeBug else ""
+    ]
+    for step in steps:
+        subprocess.run("call " + step if WIN else step, shell=True)
+
+
+if __name__ == '__main__':
+    main(get_test_part())
